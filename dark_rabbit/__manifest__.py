926b75dfb6d3967f4439460e44a24f916f7b3e3f
Tolik Klimenko <dad@picpro.net.ua>
2025-08-24 13:17:01 +0300

    Bumped modules version.

--- dark_rabbit/__manifest__.py
+++ dark_rabbit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dark Rabbit",
-    "version": "18.0.0.1.5",
+    "version": "18.0.0.1.6",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
--- test_dark_rabbit/__manifest__.py
+++ test_dark_rabbit/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "category": "Hidden",
-    "version": "18.0.0.1.1",
+    "version": "18.0.0.1.2",
     "depends": [
         "dark_rabbit",
     ],
