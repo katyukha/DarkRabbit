2c0e5fcdda098717b2638cad7a5cf37def6636bf
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2025-10-04 11:41:07 +0300

    Add exchange as entity

---
+++ dark_rabbit/models/exchange.py
@@ -0,0 +1,34 @@
+from odoo import fields, models
+
+
+class Exchange(models.Model):
+    _name = "dark.rabbit.exchange"
+    _description = "Dark Rabbit: Exchange"
+    _order = "name ASC"
+
+    connection_id = fields.Many2one(
+        comodel_name="dark.rabbit.connection",
+        required=True,
+        index=True,
+        ondelete="cascade",
+    )
+    name = fields.Char(required=True, index=True)
+
+    exchange_declare = fields.Boolean(string="Declare Exchange")
+    exchange_type = fields.Selection(
+        selection=[
+            ("topic", "Topic"),
+            ("direct", "Direct"),
+            ("fanout", "Fanout"),
+            ("headers", "Headers"),
+        ]
+    )
+    exchange_durable = fields.Boolean()
+
+    def get_exchange_config(self):
+        return {
+            "name": self.name,
+            "declare": self.exchange_declare,
+            "type": self.exchange_type,
+            "durable": self.exchange_durable,
+        }
