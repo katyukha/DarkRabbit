970198678efb18e63ef1fba5fb4c3586b87ded7e
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-06-17 16:40:24 +0300

    Added translations

--- dark_rabbit/__manifest__.py
+++ dark_rabbit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dark Rabbit",
-    "version": "18.0.0.6.0",
+    "version": "18.0.0.6.1",
     "license": "LGPL-3",
     "summary": "Rabbit from Dark Side. Brings some dark magic to your messaging.",
     "category": "Technical Settings",
